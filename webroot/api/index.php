2a6ebb332ae0d29061fa361614d6512a390f6073
Cindy <prentlikes2cookies@gmail.com>
2020-06-11 16:34:34 -0500

    Final Update

--- webroot/api/index.php
+++ webroot/api/index.php
@@ -1,7 +1,72 @@
-<?php
-/**
- * This is a template php file for your countries search.
- * Use as you will, or start over. It's up to you.
- */
- header('Content-Type: application/json');
- echo json_encode(['data' => ['Your data']]);
+<?php 
+header('Content-Type: application/json');
+
+
+function SortByPopulation($object1, $object2){ //function that sorts array by population size
+return $object1["population"] < $object2["population"];
+}
+
+
+$namePart = isset($_POST['pname'])?$_POST['pname'] : null; //gets value from ajax call
+if($namePart != null){
+
+
+//define the urls to check
+$namePart_url = "https://" .
+			"restcountries.eu/" .
+			"rest/v2/name/" . rawurlencode($namePart) .
+			"?fields=name;alpha2Code;alpha3Code;region;subregion;flag;population;languages";
+
+$nameFull_url = "https://" .
+				"restcountries.eu/" .
+				"rest/v2/name/" . 
+				rawurlencode($namePart) . 
+				"?fullText=true&fields=name;alpha2Code;alpha3Code;region;subregion;flag;population;languages";
+				
+$code_url = "https://" .
+			"restcountries.eu/" .
+			"rest/v2/alpha/" . rawurlencode($namePart) .
+			"?fields=name;alpha2Code;alpha3Code;region;subregion;flag;languages;population";
+	
+//define the headers to check before requesting data
+$part_file_header=@get_headers($namePart_url);
+$full_file_header=@get_headers($nameFull_url);
+$code_file_header=@get_headers($code_url);
+
+//if the header has an error of some kind, then check a different url. If none of them work, the input from the user is invalid
+if($full_file_header && $full_file_header[0] != 'HTTP/1.1 404 '){
+	$info_json = file_get_contents($nameFull_url);
+}
+else if($code_file_header && $code_file_header[0] != 'HTTP/1.1 400 ' && $code_file_header[0] != 'HTTP/1.1 404 '){
+	$info_json = file_get_contents($code_url);
+}
+else if($part_file_header && $part_file_header[0] != 'HTTP/1.1 404 '){
+	$info_json = file_get_contents($namePart_url);
+}
+else{
+	//error! user input is invalid
+}
+	
+if(isset($info_json)){
+	$info_array = json_decode($info_json, true);//turns json into array to sort
+		
+	//for some reason the data when searching by code instead of name is formatted differently
+	//in the REST API. This fixes that issue.
+	if(!isset($info_array[0]["population"])){
+		$fixed_array = [];
+		array_push($fixed_array, $info_array);
+		$info_array = $fixed_array;
+	}
+	
+	//sort array by function defined above
+	usort($info_array, 'SortByPopulation');
+
+	//send sorted information back to js file
+	echo json_encode($info_array);
+}
+else{
+	echo "Error. Please enter a valid Country Name, Full Name, or Code.";
+}
+}
+
+?>
--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,2 +1,100 @@
 //Write your javascript here, or roll your own. It's up to you.
 //Make your ajax call to http://localhost:8765/api/index.php here
+
+var regions = {};
+var subregions = {};
+
+Object.size = function(obj) { //code used to determine size of object. I don't claim to own this code, this specific function was found on stack exchange.
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+//formats the data for the page, fills in the html table with REST data
+function PrintResults(item, index){
+	var languages;
+	for(var i = 0; i < item.languages.length; i++){
+		if(i==0){
+			languages = item.languages[i].name;
+		}
+		else{
+			languages += ", ";
+			languages += item.languages[i].name;
+		}
+	}
+	
+	document.getElementById("information").innerHTML += "<tr><th><img src=" + item.flag + " width=500 height= 300> </img></th><th>" + "Name: " + item.name + "<br>Alpha Code 2: " + item.alpha2Code + "<br>Alpha Code 3: " + item.alpha3Code + "<br>Region: " + item.region + "<br>Subregion: " + item.subregion + "<br>Population: " + item.population + "<br>Language(s): " + languages;
+
+	//add to object key value pairs tracking regions/subregions
+	if(item.region in regions){
+			regions[item.region] ++;
+		}
+	else{
+			regions[item.region] = 1;
+		}
+		
+	if(item.subregion in subregions){
+		subregions[item.subregion] ++;
+	}
+	else{
+		subregions[item.subregion] = 1;
+	}
+		document.getElementById("information").innerHTML += " </th></tr>";
+}
+
+//prints information about # countries, regions, and subregions at bottom of page
+function PrintRegionResults(){
+	var regionCount = "";
+	var subregionCount = "";
+	for(let[key, value] of Object.entries(regions)){
+		regionCount += key + ": " + value + " <br>";
+	}
+	for(let[key, value] of Object.entries(subregions)){
+		subregionCount += key + ": " + value + " <br>";
+	}
+	document.getElementById("data").innerHTML += "<tr><th>Countries In Region " + "<br>" + regionCount + "</th><th>Countries In Subregion<br>" + subregionCount + "</th></tr>";
+
+	regions = {};
+	subregions = {};
+}
+
+//Call from html to php (and back) starts here!
+$(document).ready(function(){
+	var request;
+
+	$('#countrySearch').submit(function(e){//calls when submit button is pressed
+		e.preventDefault(); 					//prevents default form submit.
+		var $form = $(this); 					//entered form data
+		var $input = $form.find("input, select, button, textarea");
+		var serializedData = $form.serialize();
+		$input.prop("disabled", true); 				//disable input while request is happening
+   
+		request = $.ajax({    //fetches data from index.php
+			url:'http://localhost:8765/api/index.php',
+			type: "post",
+			data:serializedData,
+			dataType: 'json',
+		});
+   
+		request.done(function(response, textStatus, jqXHR){ //successful request
+			document.getElementById("data").innerHTML = "";        //clear old data on html file
+			document.getElementById("information").innerHTML = ""; //clear old data on html file
+			response.forEach(PrintResults);
+			document.getElementById("data").innerHTML = "<tr><th colspan=2>Total Countries: " + Object.size(response) + "</th></tr>";
+			PrintRegionResults();
+	   
+		});
+   
+		request.fail(function(response, textStatus, errorThrown){//errored request
+			document.getElementById("information").innerHTML = "";
+			document.getElementById("data").innerHTML = "No results. Please enter a country partial or full name, or an alpha code.";
+			console.error("Error: " + textStatus, errorThrown + " Data: " + response);
+		});
+
+		request.always(function(){
+			$input.prop("disabled", false);
+		});
+	});
+});
